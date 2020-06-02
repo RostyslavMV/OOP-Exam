5d6e110e9a24de526e5c44331717189518bea684
Rostyslav M <mrostyk55@gmail.com>
2020-06-02 17:27:59 +0300

    CloudStorage base classes

---
+++ OOP Exam/CloudStorage.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exam
+{
+    class Person
+    {
+        public string Name { get; private set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class CompanyProvider
+    {
+        public string Name { get; private set; }
+
+        public CompanyProvider(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class Catalog
+    {
+        public List<File> Files { get; private set; }
+
+        public Catalog(List<File> files)
+        {
+            Files = files;
+        }
+    }
+
+    class File
+    {
+        public string Name { get; private set; }
+        public int Size { get; private set; }
+        public FileRevision CurrentRevision { get; private set; }
+
+        public File(string name)
+        {
+            Name = name;
+            CurrentRevision = new FileRevision(DateTime.Now);
+        }
+
+        public File(string name, int size) : this(name)
+        {
+            Size = size;
+        }
+    }
+
+    class FileRevision
+    {
+        public DateTime DateOfRevision { get; private set; }
+
+        public FileRevision(DateTime revisionDate)
+        {
+            DateOfRevision = revisionDate;
+        }
+    }
+}
