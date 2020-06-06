4af41e903d695a8922f146d6e6669e60f9909ae4
Rostyslav M <mrostyk55@gmail.com>
2020-06-06 20:16:29 +0300

    Start UI

--- OOP Exam/MainWindow.xaml.cs
+++ OOP Exam/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +26,72 @@ namespace OOP_Exam
         public MainWindow()
         {
             InitializeComponent();
+            InitItems();
+            DataContext = this;
+        }
+
+        public Collection<MenuItem> MenuItems { get; } = new Collection<MenuItem>();
+
+        void InitItems()
+        {
+            MenuItems.Add(new MenuItem("Ввід даних", typeof(UserControl), "About"));
+            MenuItems.Add(new MenuItem("Циклічний список", typeof(UserControl), "About"));
+            MenuItems.Add(new MenuItem("Червоно-чорне дерево", typeof(UserControl), "About"));
+            MenuItems.Add(new MenuItem("B+ Дерево", typeof(UserControl), "About"));
+            MenuItems.Add(new MenuItem("Хеш, метод ланцюжків", typeof(UserControl), "About"));
+            MenuItems.Add(new MenuItem("Хеш, квадратичне зондування", typeof(UserControl), "About"));
+            MenuItems.Add(new MenuItem("Алгоритми сортування", typeof(UserControl), "About"));
+            MenuItems.Add(new MenuItem("Контейнер", typeof(UserControl), "About"));
+            MenuItems.Add(new MenuItem("Множина", typeof(UserControl), "About"));
+        }
+
+        Collection<int> data = new Collection<int>();
+        private int _percentDone;
+
+        int percentDone
+        {
+            get => _percentDone;
+            set
+            {
+                _percentDone = value;
+                Dispatcher.Invoke(() =>
+                { pb.Value = value; });
+            }
+        }
+
+        bool run = false;
+
+        void generateCollection()
+        {
+            int count = 1000000;
+            int percent = count / 100;
+            Random random = new Random();
+            int i;
+            for (i = 1; i < 1000000; i++)
+            {
+                if (i % percent == 0)
+                {
+                    if (!run) return;
+                    Thread.Sleep(100);
+                    percentDone = i * 100 / count;
+                }
+                data.Add(random.Next());
+            }
+        }
+
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            percentDone = 0;
+            run = true;
+            DateTime start = DateTime.Now;
+            await Task.Run(() => generateCollection());
+            Debug.WriteLine(DateTime.Now - start);
+            run = false;
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            run = false;
         }
     }
 }
---
+++ OOP Exam/MenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exam
+{
+    public class MenuItem
+    {
+        public string Text { get; set; }
+        public Type ControlType { get; set; }
+        public string About { get; set; }
+        public MenuItem(string text, Type controlType, string about)
+        {
+            Text = text;
+            ControlType = controlType;
+            About = about;
+        }
+
+    }
+}
